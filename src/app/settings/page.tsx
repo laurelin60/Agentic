976073a51efd48325fe2c9ea4d9cfc9c1c6ae425
drewsmallz <idk.contact1000@gmail.com>
2024-01-13 12:07:02 -0800

    added base pages

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Profile from "@/components/Profile";
+
+const Page = () => {
+
+    return (
+        <div>
+            <div className="text-2m font-bold text-primary sm:text-2xl">
+                Settings 
+            </div>
+            
+            <div className="absolute top-0 right-0 px-2 py-2">
+                <Profile/>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
