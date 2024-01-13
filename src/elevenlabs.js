69dc63b2ac39f01a6041b2362170dc646a16d0d6
Kevin Wu <kevinwu098@gmail.com>
2024-01-13 12:21:20 -0800

    feat: add eleven labs

---
+++ src/elevenlabs.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+// Define a function called textToSpeech that takes in a string called inputText as its argument.
+const textToSpeech = async (inputText) => {
+    // Set the API key for ElevenLabs API.
+    // Do not use directly. Use environment variables.
+    const API_KEY = "33b8637e9e6e3510b11207d528f9cee6";
+    // Set the ID of the voice to be used.
+    const VOICE_ID = "21m00Tcm4TlvDq8ikWAM";
+
+    // Set options for the API request.
+    const options = {
+        method: "POST",
+        url: `https://api.elevenlabs.io/v1/text-to-speech/${VOICE_ID}`,
+        headers: {
+            accept: "audio/mpeg", // Set the expected response type to audio/mpeg.
+            "content-type": "application/json", // Set the content type to application/json.
+            "xi-api-key": `${API_KEY}`, // Set the API key in the headers.
+        },
+        data: {
+            text: inputText, // Pass in the inputText as the text to be converted to speech.
+        },
+        responseType: "arraybuffer", // Set the responseType to arraybuffer to receive binary data as response.
+    };
+
+    // Send the API request using Axios and wait for the response.
+    const speechDetails = await axios.request(options);
+
+    // Return the binary audio data received from the API response.
+    return speechDetails.data;
+};
+
+// Export the textToSpeech function as the default export of this module.
+export default textToSpeech;
