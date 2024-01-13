2bf3cd64e177235edeb9a1ee4f71cf60ffb4650e
drewsmallz <idk.contact1000@gmail.com>
2024-01-13 11:56:04 -0800

    history page updates

---
+++ src/components/SearchQueryDisplay.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React, {useState} from 'react';
+import { Button } from './ui/button';
+
+const SearchDisplay = () => {
+    return (
+        <div className="bg-lightgrey">
+            yo
+            <Button>stop</Button>
+        </div>
+    );
+};
---
+++ src/components/history/HistoryUI.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import React from 'react';
+import { useRouter } from "next/navigation";
+import ClearHistory from './ClearHistory';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+
+interface SearchBarProps {
+  onSearch: (searchTerm: string) => void;
+}
+
+const HistoryCard = () => {
+  const router = useRouter();
+  return (
+    <div>
+        <Card className="outline">
+            <ClearHistory/> 
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+        </Card>
+        
+    </div>
+  );
+};
+
+export default HistoryCard;
