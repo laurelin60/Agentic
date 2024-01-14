ba7257f96d5f7bdb8a6a9124b72ddb9b8a2be5a0
drewsmallz <idk.contact1000@gmail.com>
2024-01-13 17:00:32 -0800

    updated loading screen (needs revision)

--- src/components/SearchQuery.tsx
+++ src/components/SearchQuery.tsx
@@ -5,11 +5,11 @@ import React, { useState } from "react";
 export const SearchQuery = () => {
   const query = "Finding a wheelchair under 100 dollars";
 
-  return <div className="">{query}</div>;
+  return <div className="text-3xl font-bold">{query}</div>;
 };
 
 export const SearchAction = () => {
   const searchAction = "searching ebay";
 
-  return <div className="">{searchAction}</div>;
+  return <div className="text-2xl">{searchAction}</div>;
 };
--- src/components/SearchQueryDisplay.tsx
+++ src/components/SearchQueryDisplay.tsx
@@ -6,28 +6,34 @@ import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import { SearchQuery, SearchAction } from "./SearchQuery";
 
 const SearchDisplay = () => {
-  const [searchingQuery, setSearching] = useState(true);
-  const [showSearching, setShowSearchingState] = useState(true);
+    const [searchingQuery, setSearching] = useState(true);
+    const [showSearching, setShowSearchingState] = useState(true);
 
-  const handleStop = () => {
-    setSearching(false);
-    setShowSearchingState(false);
-  };
+    const handleStop = () => {
+        setSearching(false);
+        setShowSearchingState(false);
+    };
 
-  return (
-    <>
-      {searchingQuery && showSearching && (
-        <div className="flex flex-col justify-center items-center bg-gray-100">
-          <div className="text-lg w-[700px] bg-gray-100 rounded-md p-3" />
-          <SearchQuery />
-          <SearchAction />
-          <Button className="mb-6" onClick={handleStop}>
-            stop
-          </Button>
-        </div>
-      )}
-    </>
-  );
+    return (
+        <>
+            {searchingQuery && showSearching && (
+                <div className="flex flex-col justify-center items-center bg-gray-100 drop-shadow-md">
+                    <div className="text-lg w-[700px] bg-gray-100 rounded-md p-3" />
+
+                    <SearchQuery />
+                    <span className="py-3">
+                        <SearchAction />
+                    </span>
+
+                    <img src="loading.gif" alt="loading gif" className="flex h-16 w-16 justify-center opacity-60 mb-6"/>
+
+                    <Button className="mb-6 text-2xl" onClick={handleStop}>
+                        stop
+                    </Button>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default SearchDisplay;
