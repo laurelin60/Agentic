ffa71894dfe5d94ed44616afeef490fae0a81975
drewsmallz <idk.contact1000@gmail.com>
2024-01-13 17:04:56 -0800

    hide viewer toggle until searching

--- src/components/ViewerToggle.tsx
+++ src/components/ViewerToggle.tsx
@@ -4,26 +4,30 @@ import React, { useState } from "react";
 import { Switch } from "./ui/switch";
 
 interface SearchBarProps {
-  onSearch: (searchTerm: string) => void;
+    onSearch: (searchTerm: string) => void;
 }
 
-const ToggleViewer = () => {
-  const [toggled, setToggled] = useState(false);
+const ToggleViewer = (props: { searchingQuery: any }) => {
+    const [toggled, setToggled] = useState(false);
 
-  const handleClick = () => {
-    if (toggled) {
-      setToggled(false);
-    } else {
-      setToggled(true);
-    }
-  };
+    const handleClick = () => {
+        if (toggled) {
+            setToggled(false);
+        } else {
+            setToggled(true);
+        }
+    };
 
-  return (
-    <div className="flex">
-      <div className="pr-2">viewer</div>
-      <Switch id="toggle viewer" onClick={handleClick} />
-    </div>
-  );
+    return (
+        <>
+            {props.searchingQuery && (
+                <div className="flex">
+                    <div className="pr-2">viewer</div>
+                    <Switch id="toggle viewer" onClick={handleClick} />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default ToggleViewer;
